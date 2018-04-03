b402fa186f875ccacabe8b8cae27a4dbd320fdd0
Sapir Bar-On <sapirteri@gmail.com>
2018-04-03 14:33:21 +0300

    sapir client code

---
+++ app/data.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { Http , Response} from '@angular/http';
+
+@Injectable()
+export class DataService {
+
+  constructor(private http: Http) { }
+
+   allUsers(callback: Function) {
+      this.http.get('http://localhost:3000/getAllData')
+      .subscribe(
+          (res: Response ) => {
+              callback( res.json() );
+          }
+       )
+  }
+
+  login(email:string,password:string,callback: Function) {
+      this.http.post('http://localhost:3000/login',{'email':email,'password':password})
+      .subscribe(
+          (res: Response ) => {
+              callback( res.json() );
+          },
+          (error => {
+            console.log(error);
+            callback(null);
+        })
+       );
+  }
+
+  createUser(firstName:string,lastName:string,email:string,password:string,callback: Function){
+           this.http.post('http://localhost:3000/createNewAccount',
+           {'firstName':firstName,'lastName':lastName,'email':email,'password':password})
+              .subscribe(
+                  (res: Response ) => {
+                      callback( res.json() );
+                  },
+                  (error => {
+                    console.log(error);
+                    callback(null);
+                })
+               );
+    }
+
+
+}
