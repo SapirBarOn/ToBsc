92eb4dc1523a40d6fcdb957732fdc7d336fa443d
shiran44 <shirant442@gmail.com>
2018-04-03 10:41:09 +0300

    Add files via upload

---
+++ Client/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {Routes, RouterModule } from '@angular/router';
+
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import  { DataService } from './data.service';
+import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AppMainComponent } from './app-main/app-main.component';
+import { AuthComponent } from './app-main/auth/auth.component';
+import { LoginComponent } from './app-main/login/login.component';
+import { EnterComponent } from './app-main/enter/enter.component';
+
+
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/enter', pathMatch: 'full'},
+  {path: 'enter', component: EnterComponent},
+  {path: 'auth', component: AuthComponent},
+  {path: 'login', component: LoginComponent},
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppHeaderComponent,
+    AppFooterComponent,
+    AppMainComponent,
+    AuthComponent,
+    LoginComponent,
+    EnterComponent
+  ],
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+  ],
+  exports: [RouterModule],
+  providers: [DataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
