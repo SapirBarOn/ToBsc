e0aef59d55e76580bc6f50839cb72ccf13d1209c
shiran44 <shirant442@gmail.com>
2018-06-03 15:31:45 +0300

    Update Qustion.model.ts

--- Client/src/app/model/Qustion.model.ts
+++ Client/src/app/model/Qustion.model.ts
@@ -9,9 +9,9 @@ export class Question {
     public Wmanagement: number;
     public Wbuilding: number;
     public Wmachine: number;
-
+    public Wdifferent: number;
   constructor(questionId:number,questionData: string,Wchemistry: number,Wsoftware: number,
-      Welectronic: number,Wmedical: number,Wmanagement: number,Wbuilding: number,Wmachine: number){
+      Welectronic: number,Wmedical: number,Wmanagement: number,Wbuilding: number,Wmachine: number,Wdifferent: number){
     this.questionId = questionId;
     this.questionData=questionData;
     this.Wchemistry=Wchemistry;
@@ -21,6 +21,8 @@ export class Question {
     this.Wmedical=Wmedical;
     this.Wsoftware=Wsoftware;
     this.Wbuilding=Wbuilding;
+    this.Wdifferent=(Math.max(Wsoftware,Wmedical,Wchemistry,Wbuilding,Welectronic,Wmachine,Wmanagement))-(Math.min(Wsoftware,Wmedical,Wchemistry,Wbuilding,Welectronic,Wmachine,Wmanagement));
+    this.Wdifferent=Wdifferent;
   }
 }
 
