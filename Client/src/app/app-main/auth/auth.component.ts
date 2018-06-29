4cef93fa5d576805cddeec5cde6379723195bdf7
shiran44 <shirant442@gmail.com>
2018-06-29 17:01:54 +0300

    Update auth.component.ts

--- Client/src/app/app-main/auth/auth.component.ts
+++ Client/src/app/app-main/auth/auth.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import { CurrentUser } from '../../app-shared/current-user';
 import { FormGroup ,FormControl ,FormBuilder } from '@angular/forms';
 import { NgbModal , ModalDismissReasons ,NgbAlertConfig} from '@ng-bootstrap/ng-bootstrap';
+import { User } from '../../model/user.model';
+
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
@@ -13,6 +15,7 @@ export class AuthComponent implements OnInit {
 
     response:String;
     auth: FormGroup;
+    user:User;
     @ViewChild('firstName') firstNameInputRef : ElementRef;
     @ViewChild('lastName') lastNameInputRef : ElementRef;
     @ViewChild('email') emailInputRef : ElementRef;
@@ -58,9 +61,19 @@ export class AuthComponent implements OnInit {
                this.ageInputRef.nativeElement.value,result=>{
 
                 if(result){
-                    this.dataService.myMethod(result); 
-                    this.currentUserService.change(result);
-                    this.router.navigateByUrl('/enter');
+                  console.log(result._id);
+                let user = new User();
+                user.setId(result._id);
+                user.setFirstName(result.firstName);
+                user.setEmail(result.email);
+                user.setLastName(result.lastName);  
+                user.setPassword(result.password); 
+                user.setAge(result.age);
+                user.setWorkExperience(result.WorkExperience);
+                user.setGender(result.gender); 
+                this.currentUserService.change(user); 
+                this.dataService.myMethod(result); 
+                this.router.navigateByUrl('/enter'); 
                 }
                 else{
                     document.getElementById('res').innerHTML='ישנה שגיאה,אנא נסה שנית';
