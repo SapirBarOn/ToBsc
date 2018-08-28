2ded3edf210794d3afb2255a21cfc7e240b28773
shanizil <shanizil100@walla.com>
2018-08-29 00:50:12 +0300

    Add files via upload

---
+++ Client/src/app/model/Logs.model.ts
@@ -0,0 +1,10 @@
+export class Logs {
+    public logDate:string;
+    public logData: string;
+
+  constructor(logDate:string,logData: string,){
+    this.logDate = logDate;
+    this.logData=logData;
+
+  }
+}
