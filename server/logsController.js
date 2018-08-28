cb6ed5cee0e4d31dd5eed999e217e2bc7065a62e
shanizil <shanizil100@walla.com>
2018-08-28 22:49:45 +0300

    Add files via upload

--- server/logsController.js
+++ server/logsController.js
@@ -11,66 +11,41 @@ const   mongoose = require('mongoose'),
         };
 
 
-// exports.getAllLogs = function(req, res){
-//         Logs.findOne({}).where('logsType').equals(req.body.logsType).
-//         exec (function(err, data){
-//             if(err) {
-//                 console.log(err);
-//                 return res.status(500).send();
-//             }
-//             // console.log(JSON.stringify(data.logsArr, null, 2));
-//             console.log("Get Logs Successfully");
-//             return res.status(200).send(data.logsArr);
-//         })
-
-//          return res.status(404).send();
-// };
-
 module.exports={
 
-
         getAllLogs(req,res){
-        logs.findOne({logsType : req.body.logsType}, (err,result)=>{
-            if(err || !result){
-                return res.status(500).json(`{logs not exists:${err}}`);
+            return logs.find();
+        },
+
+        getTypeLogs(req,res){
+            logs.findOne({logType : req.body.type}, (err,result)=>{
+                 if(err){
+                    return res.status(500).json(`{error:${err}}`);
+                }if(!result){
+                    return res.status(201).json(`{logs not exists:${err}}`);
+                }
+                else {
+                    console.log("Get Logs Successfully");
+                    return res.status(200).json(result);
+                }
+            });
+        },
+
+    getTypeLog(req,res){
+        if(req.body.logsType){
+        logs.findOne({}).where('logsType').equals(req.body.logsType).
+        exec (function(err, data){
+            if(err) {
+                console.log(err);
+                return res.status(500).send();
             }
-            else {
-                console.log("Get Logs Successfully");
-                return res.status(200).send(result.logsArr);
-            }
-        });
-     }
-    //     getAllLogs(){
-    //         return Logs.findOne();
-    //     }
-
-       // getAllUserLogin(){
-       //  return logs.find();
-       // },
-
-       // getAllrefreshErr(){
-       //   return logs.find();
-       // },
-
-       // getAllrefreshLogs(){
-       //  return logs.find();
-       // }
+            console.log(JSON.stringify(data.logsArr, null, 2));
+            console.log("Get Logs Successfully");
+            return res.status(200).send(data.logsArr);
+        })
+    }
+    else return res.status(404).send();
+    }
 };
 
 
-// Get Logs by Type (Admin Mode)
-// exports.getLogs = function(req, res){
-//     if(req.body.logsType){
-//         Logs.findOne({}).where('logsType').equals(req.body.logsType).
-//         exec (function(err, data){
-//             if(err) {
-//                 console.log(err);
-//                 return res.status(500).send();
-//             }
-//            // console.log(JSON.stringify(data.logsArr, null, 2));
-//             console.log("Get Logs Successfully");
-//             return res.status(200).send(data.logsArr);
-//         })
-//     }
-//     else return res.status(404).send();
-// };
